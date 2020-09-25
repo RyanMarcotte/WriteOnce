d8be6d2a8395eeb9efa5019ecea6ee2b81da29ea
Ryan Marcotte <ryan.marcotte@live.ca>
2020-09-25 00:04:43 -0600

    initial commit

---
+++ src/WriteOnce/WriteOnce/ValueNotSetException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WriteOnce
+{
+	/// <summary>
+	/// Exception thrown from <see cref="WriteOnce{T}"/> when the value has not been set yet.
+	/// </summary>
+	public class ValueNotSetException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ValueNotSetException"/> class.
+		/// </summary>
+		internal ValueNotSetException()
+			: base($"Attempted to retrieve a value from '{typeof(WriteOnce<>)}', but value not set!")
+		{
+
+		}
+	}
+}
